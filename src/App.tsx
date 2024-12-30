a74d2cbb7c41cc72022f454afc4b297f8a60aa01
agusbrocani <aguss.brocani@gmail.com>
2024-12-29 23:43:44 -0300

    Añado test para aplicar accion a Objeto recursivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import ObjetosEnProfundidad from './ObjetosEnProfundidad'
 import Matriz from './Matriz'
+import PruebaAplicarAccionEnProfundidadEnObjeto from './PruebaAplicarAccionEnProfundidadEnObjeto'
 
 function App() {
   return (
     <>
-        <ObjetosEnProfundidad/>
-        <Matriz/>
+        {/* <ObjetosEnProfundidad/>
+        <Matriz/> */}
+        <PruebaAplicarAccionEnProfundidadEnObjeto/>
     </>
   )
 }
